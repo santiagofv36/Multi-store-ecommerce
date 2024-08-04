d7b3c03086b3203d481fb99b7df751f9e3db8c75
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-08-04 03:37:31 -0400

    chore(api): :ambulance: adding guards to store endpoints

--- apps/api/src/components/store/store.controller.ts
+++ apps/api/src/components/store/store.controller.ts
@@ -1,7 +1,15 @@
-import { Body, Controller, UsePipes, Post, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  UsePipes,
+  Post,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
 import { StoreService } from './store.service';
 import { ZodValidationPipe } from 'src/core/pipes';
 import { createStoreInput, TCreateStoreInput } from '@packages/models';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('store')
 export class StoreController {
@@ -9,6 +17,7 @@ export class StoreController {
 
   @Post('')
   @UsePipes(new ZodValidationPipe(createStoreInput))
+  @UseGuards(AuthGuard('jwt'))
   async create(@Body() data: TCreateStoreInput) {
     return await this.storeService.createStore(data);
   }
