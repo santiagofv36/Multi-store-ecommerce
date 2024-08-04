0f02dd7a13158458fb264c0d81866615f7b2674a
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-08-04 02:58:43 -0400

    feat(admin): :sparkles: adding store modal functionallity

--- apps/admin/src/components/modals/store-modal.tsx
+++ apps/admin/src/components/modals/store-modal.tsx
@@ -1,10 +1,37 @@
 'use client';
 
+import React from 'react';
 import { useStoreModal } from '@admin/hooks/use-store-modeal';
 import Modal from '@admin/components/ui/modal';
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
+import { IStore, storeDefinition, TCreateStore } from '@packages/models';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import StoreForm from '../forms/store-form';
 
 export default function DashboardPage() {
   const { isOpen, close } = useStoreModal();
+
+  const [disabled, setDisabled] = React.useState<boolean>(false);
+
+  const form = useForm<IStore>({
+    resolver: zodResolver(storeDefinition),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (data: TCreateStore) => {
+    try {
+      setDisabled(true);
+      console.log(data);
+    } catch (error) {
+    } finally {
+      setDisabled(false);
+    }
+  };
+
   return (
     <Modal
       title="Create Store"
@@ -12,7 +39,15 @@ export default function DashboardPage() {
       onClose={close}
       isOpen={isOpen}
     >
-      Hi
+      <div>
+        <div className="space-y-4 py-3 pb-4">
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <StoreForm disabled={disabled} onCancel={close} />
+            </form>
+          </FormProvider>
+        </div>
+      </div>
     </Modal>
   );
 }
