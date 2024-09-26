5037c5b41e5d8d622e5d53b195af2eb5d3f9a184
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-09-25 22:53:11 -0400

    fix(api): :zap: fixing user schema

--- packages/models/src/user/user.schema.ts
+++ packages/models/src/user/user.schema.ts
@@ -10,7 +10,8 @@ export type UserDocument = IUser & Document<Types.ObjectId, object, IUser>;
 export const userSchema = new Schema<IUser>(
   {
     email: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     password: { type: String, required: true },
     activeSession: sessionSchema,
     role: {
