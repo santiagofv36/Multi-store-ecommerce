e443484424ddf729e825495b4c1e16bf9638be2c
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-10-03 22:57:34 -0400

    feat(admin): :sparkles: adding billboard hooks

--- apps/admin/src/services/billboard/hooks.ts
+++ apps/admin/src/services/billboard/hooks.ts
@@ -1,6 +1,6 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import { TFindOneBillboardInput } from 'packages/models/src';
+import { useMutation, useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { getBillboard } from './queries';
+import { createBillboard, deleteBillboard, updateBillboard } from '.';
 
 export function useBillboard(
   params: { _id: string },
@@ -10,5 +10,26 @@ export function useBillboard(
     queryKey: ['getBillboard', params],
     queryFn: () => getBillboard(params),
     ...options,
+  });
+}
+
+export function useCreateBillboard() {
+  return useMutation({
+    mutationKey: ['createBillboard'],
+    mutationFn: createBillboard,
+  });
+}
+
+export function useUpdateBillboard() {
+  return useMutation({
+    mutationKey: ['updateBillboard'],
+    mutationFn: updateBillboard,
+  });
+}
+
+export function useDeleteBillboard() {
+  return useMutation({
+    mutationKey: ['deleteBillboard'],
+    mutationFn: deleteBillboard,
   });
 }
--- apps/admin/src/services/billboard/mutations.ts
+++ apps/admin/src/services/billboard/mutations.ts
@@ -1,1 +1,31 @@
-export{}
+import { api } from '@admin/lib/api';
+import {
+  IBillboard,
+  TCreateBillboardInput,
+  TFilterBillboardsInput,
+  TFindOneBillboardInput,
+} from 'packages/models/src';
+
+export async function createBillboard(input: TCreateBillboardInput) {
+  const { data } = await api.post<IBillboard>({
+    url: '/billboard',
+    options: { body: JSON.stringify({ ...input, active: true }) },
+  });
+
+  return data;
+}
+export async function updateBillboard(input: TFilterBillboardsInput) {
+  const { data } = await api.put<IBillboard>({
+    url: '/billboard',
+    options: { body: JSON.stringify(input) },
+  });
+
+  return data;
+}
+export async function deleteBillboard(input: TFindOneBillboardInput) {
+  const { data } = await api.delete<IBillboard>({
+    url: `/billboard/${input._id}`,
+  });
+
+  return data;
+}
